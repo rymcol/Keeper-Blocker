5ce9f0633f5a621da059f2a6ba7fa0cded8595ea
Ryan M. Collins <ryan@ryanmcollins.com>
2015-08-21 13:32:52 -0400

    initial commit

---
+++ Keeper Blocker.safariextension/keeperblocker.js
@@ -0,0 +1,17 @@
+var theURI = window.location.href;
+
+var itsTheKeeper = theURI.match('mackeeper\.(com|net|org)');
+
+if (itsTheKeeper) {
+
+		var theDocList = document.getElementsByTagName('html');
+	
+		theDocList.length;
+	
+		theDocList[0].style.display = 'none';
+		
+		if (confirm("This website may be harmful and will be closed")) {
+			var theMessage = "close-tab";
+			safari.self.tab.dispatchMessage("command", theMessage);
+  }
+}
